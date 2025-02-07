ae0d4183ed33ab1b90f09a4111b0bdc6e2078552
Nithin Jakrebet <nithinjbet@gmail.com>
2025-02-07 15:26:26 -0700

    State vector development

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import numpy as np
 
 def evaluate_model(y_true, y_pred) -> dict:
     """
@@ -6,7 +7,7 @@ def evaluate_model(y_true, y_pred) -> dict:
     Prints them and returns a dictionary of the metrics.
     """
     mse = mean_squared_error(y_true, y_pred)
-    rmse = mean_squared_error(y_true, y_pred, squared=False)
+    rmse = np.sqrt(mse)
     r2 = r2_score(y_true, y_pred)
     mae = mean_absolute_error(y_true, y_pred)
 
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-def preprocess_data(df: pd.DataFrame, target_col: str, feature_cols: list):
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Preprocesses the dataset by dropping NaN values, filtering relevant rows,
     sampling, and scaling the features and target.
@@ -25,16 +25,13 @@ def preprocess_data(df: pd.DataFrame, target_col: str, feature_cols: list):
     if "#NAME?" in df.columns.values : df = df.rename(columns={"#NAME?": "Im(Z)/Ohm"})
 
     # Apply filtering
-    df = df.loc[(df['Ns'].isin([1, 6])) & (df['cycle number'] != 0)].copy()
-
-    # Extract features and target
-    X = df[feature_cols].values
-    y = df[target_col].values
-
-    # Scale features and target
-    scaler_X = StandardScaler()
-    scaler_y = StandardScaler()
-    X_scaled = scaler_X.fit_transform(X)
-    y_scaled = scaler_y.fit_transform(y.reshape(-1, 1)).ravel()
-
-    return X_scaled, y_scaled, scaler_X, scaler_y
+    # df = df.loc[(df['cycle number'] != 0)].copy()
+    # df = df.loc[(df['Ns'].isin([1, 6])) & (df['cycle number'] != 0)].copy()
+    
+    # Split dataframe by cycle numbers
+    # dataframe_dict = {}
+    # cycle_nums = df['cycle number'].unique()
+    # for cycle in cycle_nums:
+    #    dataframe_dict[f'df_cycle_{cycle}'] = df[df['cycle number'] == cycle].copy()
+    
+    return df
