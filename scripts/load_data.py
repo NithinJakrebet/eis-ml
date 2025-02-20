95926cfd58c0aa3da3e08e248f986ca643450de3
Nithin Jakrebet <nithinjbet@gmail.com>
2025-02-19 20:48:05 -0700

    feat: preprocess sav

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -4,18 +4,6 @@ import pandas as pd
 def load_data(subfolder: str, filename: str, data_dir: str = "../data") -> pd.DataFrame:
     """
     Loads a CSV file from a given subfolder in the data directory.
-
-    Parameters:
-        subfolder (str): The name of the subfolder (e.g., "03-06-24").
-        filename (str): The name of the CSV file to load (e.g., "A1.csv").
-        data_dir (str, optional): The base directory containing data subfolders. Defaults to "../data".
-
-    Returns:
-        pd.DataFrame: The loaded DataFrame.
-
-    Raises:
-        FileNotFoundError: If the file does not exist.
-        ValueError: If an error occurs while reading the file.
     """
 
     file_path = os.path.join(data_dir, subfolder, filename)
